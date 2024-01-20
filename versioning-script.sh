be661cc32beb2abcd4886c5744fac9b4a83694ce
Daniel Ola-Olorun <danielolaolorun@gmail.com>
2024-01-20 14:17:52 -0800

    updated build script for image versioning

---
+++ versioning-script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Get the latest tag
+latest_tag=$(git describe --tags --abbrev=0)
+
+# Split the version components
+IFS='.' read -r major minor patch <<< "$latest_tag"
+
+# Increment the patch version
+patch=$((patch + 1))
+
+# Output the new version
+echo "${major}.${minor}.${patch}"
