702711d908ee40d2b156f1bbd95170a70fe06ef0
Daniel Ola-Olorun <danielolaolorun@gmail.com>
2024-01-27 19:12:18 -0800

    test

--- versioning-script.sh
+++ versioning-script.sh
@@ -7,7 +7,7 @@ latest_tag=$(git describe --tags --abbrev=0)
 IFS='.' read -r major minor patch <<< "$latest_tag"
 
 # Increment the patch version
-patch=$((patch + 1))
+#patch=$((patch + 1))
 
 # Output the new version
 echo "$major.$minor.$patch"
